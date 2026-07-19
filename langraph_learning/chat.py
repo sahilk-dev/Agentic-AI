a7a1919d95a1090963712a3d48e29fbd0e5a7bd8
Sahil Kamila <sahilkamila143@gmail.com>
2026-07-19 18:39:39 +0530

    Integrate AI LLMs into langGraph

---
+++ langraph_learning/chat.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+llm = init_chat_model(
+    model="gemini-2.5-flash",
+    model_provider="google_genai"
+)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def chatbot(state: State):
+    response = llm.invoke(state.get("messages"))
+    return { "messages": [response] }
+
+def samplenode(state: State):
+    print("\n\nInside samplenode node", state)
+    return { "messages": ["Sample Message Appended"] }
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("samplenode", samplenode)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", "samplenode")
+graph_builder.add_edge("samplenode", END)
+
+graph = graph_builder.compile()
+
+updated_state = graph.invoke(State({"messages": ["Hi, My name is Sahil Kamila"]}))
+print("\n\nupdated_state", updated_state)
+
+# (START) -> chatbot -> samplenode -> (END)
+
+# state = { messages: ["Hey there"] }
+# node runs: chatbot(state: ["Hey There"]) -> ["Hi, This is a message from Chatbot Node"]
+# state = { "messages": ["Hey There"]) -> ["Hi, This is a message from Chatbot Node"] }
