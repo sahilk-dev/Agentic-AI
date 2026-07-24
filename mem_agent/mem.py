172b70d944a945b8caab92e070d085e244b5a848
Sahil Kamila <sahilkamila143@gmail.com>
2026-07-24 10:26:04 +0530

    The memory layer in AI Agents

---
+++ mem_agent/mem.py
@@ -0,0 +1,71 @@
+from dotenv import load_dotenv
+from mem0 import Memory
+import os
+
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+config = {
+    "version": "v1.1",
+    "embedder": {
+        "provider": "openai",
+        "config": { "api_key": OPENAI_API_KEY, "model": "text-embedding-3-small" }
+    },
+    "llm": {
+        "provider": "openai",
+        "config": { "api_key": OPENAI_API_KEY, "model": "gpt-4.1" }
+    },
+    "vector_store": {
+        "provider": "quadrant",
+        "config": {
+            "host": "localhost",
+            "port": 6333
+        }
+    }
+}
+
+mem_client = Memory.from_config(config)
+
+
+while True:
+
+    user_query = input("> ")
+
+    search_memory = mem_client.search(query=user_query, user_id="sahilkamila")
+
+    memories = [
+        f"ID: {mem.get("id")}\nMemory: {mem.get("memory")}" for mem in search_memory.get("results")
+    ]
+
+    print("Found Memories", memories)
+
+    SYSTEM_PROMPT = f"""
+    Here is the context about the user:
+    {json.dumps(memories)}
+    """ 
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            { "role": "system", "content": SYSTEM_PROMPT },
+            { "role": "user", "content": user_query }
+        ]
+    )
+
+    ai_response = response.choices[0].message.content
+
+    print("AI:", ai_response)
+
+    mem_clientuser_id="sahilkamila".add(
+        user_id="sahilkamila",
+        messages=[
+            { "role": "user", "content": user_query },
+            { "role": "assistant", "content": ai_response }
+        ]
+    )
+
+    print("Memory has been saved...")
