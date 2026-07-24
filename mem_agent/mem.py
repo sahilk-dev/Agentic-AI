65c67f0763e0433d9fd42fd2fb502c45015fed53
Sahil Kamila <sahilkamila143@gmail.com>
2026-07-24 21:37:04 +0530

    Graph Memory and Knowledge Graph

--- mem_agent/mem.py
+++ mem_agent/mem.py
@@ -1,29 +1,47 @@
 from dotenv import load_dotenv
 from mem0 import Memory
 import os
+import json
 
-from openai import OpenAI
+from google import genai
 
 load_dotenv()
 
-client = OpenAI()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 config = {
     "version": "v1.1",
     "embedder": {
-        "provider": "openai",
-        "config": { "api_key": OPENAI_API_KEY, "model": "text-embedding-3-small" }
+        "provider": "gemini",
+        "config": {
+            "api_key": GEMINI_API_KEY,
+            "model": "models/gemini-embedding-001",
+            "embedding_dims": 768
+        }
     },
     "llm": {
-        "provider": "openai",
-        "config": { "api_key": OPENAI_API_KEY, "model": "gpt-4.1" }
+        "provider": "gemini",
+        "config": {
+            "api_key": GEMINI_API_KEY,
+            "model": "gemini-2.5-flash"
+        }
+    },
+    "graph_store": {
+        "provider": "neo4j",
+        "config": {
+            "url": "neo4j+s://8e211fa8.databases.neo4j.io",
+            "username": "8e211fa8",
+            "password": "tNjuuc0f04P4a_-McXSPLxjIdjXEU3ZqgoQPjgJ5ahM"
+        }
     },
     "vector_store": {
-        "provider": "quadrant",
+        "provider": "qdrant",
         "config": {
             "host": "localhost",
-            "port": 6333
+            "port": 6333,
+            "embedding_model_dims": 768
         }
     }
 }
@@ -35,10 +53,13 @@ while True:
 
     user_query = input("> ")
 
-    search_memory = mem_client.search(query=user_query, user_id="sahilkamila")
+    search_memory = mem_client.search(
+        query=user_query,
+        filters={"user_id": "sahilkamila"}
+    )
 
     memories = [
-        f"ID: {mem.get("id")}\nMemory: {mem.get("memory")}" for mem in search_memory.get("results")
+        f"ID: {mem.get('id')}\nMemory: {mem.get('memory')}" for mem in search_memory.get("results")
     ]
 
     print("Found Memories", memories)
@@ -46,21 +67,18 @@ while True:
     SYSTEM_PROMPT = f"""
     Here is the context about the user:
     {json.dumps(memories)}
-    """ 
+    """
 
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            { "role": "system", "content": SYSTEM_PROMPT },
-            { "role": "user", "content": user_query }
-        ]
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"{SYSTEM_PROMPT}\n\nUser: {user_query}"
     )
 
-    ai_response = response.choices[0].message.content
+    ai_response = response.text
 
     print("AI:", ai_response)
 
-    mem_clientuser_id="sahilkamila".add(
+    mem_client.add(
         user_id="sahilkamila",
         messages=[
             { "role": "user", "content": user_query },
